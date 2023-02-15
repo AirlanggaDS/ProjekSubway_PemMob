3af7e28aece5314ad8087d2558ff2ae3e1798d2f
rayhaaanm <93880193+rayhaaanm@users.noreply.github.com>
2023-02-15 14:38:56 +0700

    Add files via upload

---
+++ lib/home.dart
@@ -0,0 +1,273 @@
+import 'package:easy_search_bar/easy_search_bar.dart';
+import 'package:flutter/material.dart';
+import './profil.dart';
+import './menu.dart';
+
+void main() {
+  runApp(Home());
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, home: new HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Colors.amber,
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(0, 151, 67, 1),
+        title:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Container(
+            child: Image.asset(
+              "images/logo1.png",
+              width: 30,
+              height: 30,
+            ),
+          ),
+          Row(children: [
+            Container(
+              child: const Text(
+                "Hello User,",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'LexendDeca',
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent, elevation: 0),
+              child: CircleAvatar(
+                backgroundColor: Colors.transparent,
+                radius: 10,
+                child: Icon(
+                  Icons.person_rounded,
+                  color: Colors.amber,
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Profile()));
+              },
+            )
+          ])
+        ]),
+      ),
+      body: ListView(
+        children: [
+          Container(
+              height: 200,
+              child: Image.asset(
+                "images/subway2.png",
+                fit: BoxFit.cover,
+              )),
+          Container(
+            margin:
+                const EdgeInsets.only(left: 15, top: 10, bottom: 20, right: 15),
+            width: 100,
+            height: 50,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 100,
+                  offset: Offset(0, 4), // Shadow position
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text(
+                    "Pencarian",
+                    style: TextStyle(
+                      fontFamily: 'LexendDeca',
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                // EasySearchBar(
+                //   title: Text("Pencarian") ,
+                //   onSearch: (value) => setState(() => searchValue = value )),
+                // IconButton(onPressed: () {}, icon: Icon(Icons.search), color: Colors.grey,)
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Container(
+                    width: 142,
+                    height: 63,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 10, left: 5),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Saldo",
+                              style: TextStyle(
+                                  fontFamily: 'LexendDeca', fontSize: 15)),
+                          Text(
+                            "Rp. 57.000",
+                            style: TextStyle(
+                                fontFamily: 'LexendDeca',
+                                fontSize: 15,
+                                fontWeight: FontWeight.w600),
+                          ),
+                        ],
+                      ),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 30),
+                child: Container(
+                    width: 142,
+                    height: 63,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 10, left: 5),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Point Tersedia",
+                              style: TextStyle(
+                                fontFamily: 'LexendDeca',
+                                fontSize: 15,
+                              )),
+                          Text(
+                            "65",
+                            style: TextStyle(
+                                fontFamily: 'LexendDeca',
+                                fontSize: 15,
+                                fontWeight: FontWeight.w600),
+                          ),
+                        ],
+                      ),
+                    )),
+              )
+            ],
+          ),
+          Divider(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Container(
+                    width: 142,
+                    height: 142,
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: FittedBox(
+                      child: Image.asset(
+                        "images/Group_1.png",
+                        fit: BoxFit.fill,
+                      ),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 30),
+                child: Container(
+                    width: 142,
+                    height: 142,
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: FittedBox(
+                      child: Image.asset(
+                        "images/Group_2.png",
+                        fit: BoxFit.fill,
+                      ),
+                    )),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Container(
+                    width: 142,
+                    height: 142,
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: FittedBox(
+                      child: Image.asset(
+                        "images/Group_2.png",
+                        fit: BoxFit.fill,
+                      ),
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 30),
+                child: Container(
+                    width: 142,
+                    height: 142,
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: FittedBox(
+                      child: Image.asset(
+                        "images/Group_1.png",
+                        fit: BoxFit.fill,
+                      ),
+                    )),
+              )
+            ],
+          )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color.fromRGBO(0, 151, 67, 1),
+        selectedItemColor: Colors.amber,
+        unselectedItemColor: Colors.white,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_book),
+            label: 'Menu',
+            
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.wallet_giftcard),
+            label: 'Reward',
+          ),
+        ],
+        currentIndex: 0,
+        
+      ),
+    );
+  }
+}
---
+++ lib/profil.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+
+
+class Profile extends StatelessWidget{
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(0, 151, 67, 1),
+      ),
+      body: 
+      ListView(
+        children: [
+          Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(left: 15, top: 10, bottom: 20, right: 15),
+                height: 87,
+                width: 99,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: 
+                Image.asset("images/pfp.jpg",fit: BoxFit.contain,),
+              ),
+              const Text("User", style: TextStyle(fontFamily: 'LexendDeca', fontSize: 26, color: Colors.black),),
+              Container(
+                width: 365,
+                height: 150,
+                decoration: const BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(10),topRight: Radius.circular(10)),
+                ),
+                child: 
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.only(left: 30),
+                      child: Text("Saldo", style: TextStyle(
+                        fontFamily: 'LexendDeca',
+                        fontSize: 30,
+                        color: Colors.black,
+                      ),),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 30, top: 20),
+                      child: Text("Rp 57.000", style: TextStyle(
+                        fontFamily: 'LexendDeca',
+                        fontSize: 30,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600,
+                      ),),
+                    ),
+                  ],
+                )
+              ),
+              Container(
+                width: 365,
+                height: 100,
+                decoration: const BoxDecoration(
+                  color: Color.fromRGBO(0, 151, 67, 1),
+                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10),bottomRight: Radius.circular(10)),
+                ),
+                child: 
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.only(left: 30),
+                      child: 
+                      Text("Point Tersedia ", style: TextStyle(
+                        fontFamily: 'LexendDeca',
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 30, top: 20),
+                      child: 
+                      Text("65 ", style: TextStyle(
+                        fontFamily: 'LexendDeca',
+                        fontSize: 30,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                      ),),
+                    ),
+                  ],
+                )  
+              ),
+              Divider(),
+             Container(
+                width: 365,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10),
+                ), 
+             ),
+             Divider(),
+             Container(
+                width: 365,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10),
+                ), 
+             ),
+             Divider(),
+             Container(
+                width: 365,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10),
+                ), 
+             ),
+             Divider(),
+            ],
+          )
+        ],
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     Navigator.of(context).push(
+      //       MaterialPageRoute(
+      //         builder: (context){
+      //           return Home();
+      //         },
+      //       ),
+      //       );
+      //   },
+      //   child: 
+      //   Icon(Icons.arrow_back_ios),
+      // ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.miniStartDocked,
+      // ),
+    );
+    
+  }
+}
