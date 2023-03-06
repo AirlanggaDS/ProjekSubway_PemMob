a752511d82083ff754cc9cde1e11b49384ffd5bc
rayhaaanm <rayhan.azazaka.firdaus-2021@fst.unair.ac.id>
2023-03-07 00:27:25 +0700

    nambah sesuatu yg keren

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:projek_kelompok/pages/Login.dart';
+import 'package:projek_kelompok/pages/StartApp.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Login(),
+    home: Start(),
   ));
 }
 
---
+++ lib/pages/StartApp.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:projek_kelompok/pages/Login.dart';
+
+class Start extends StatefulWidget {
+  const Start({super.key});
+
+  @override
+  State<Start> createState() => _StartState();
+}
+
+class _StartState extends State<Start> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), 
+    ()=>Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context)=>Login())
+    ));
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 3, 178, 58),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('images/subway_logo.png', width: 250,height: 250,),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.amber),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
